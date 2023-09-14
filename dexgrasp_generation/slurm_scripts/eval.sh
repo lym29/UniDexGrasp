a03f2a57a77d9596d4ddde60f5509fc47f12acf0
lym29 <lym29@connect.hku.hk>
2023-09-15 06:09:35 +0800

    add eval.sh

--- dexgrasp_generation/slurm_scripts/eval.sh
+++ dexgrasp_generation/slurm_scripts/eval.sh
@@ -2,5 +2,6 @@
 
 source /usr/miniconda3/etc/profile.d/conda.sh
 conda activate unidexgrasp
+cd /public/home/v-liuym/projects/UniDexGrasp/dexgrasp_generation
 python ./network/eval.py  --config-name eval_config \
                           --exp-dir=./eval
