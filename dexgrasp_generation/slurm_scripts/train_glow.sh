ad183e434f74adfb7b8b15c17546cce71f393d80
lym29 <lym29@connect.hku.hk>
2023-09-17 18:50:31 +0800

    modify eval

---
+++ dexgrasp_generation/slurm_scripts/train_glow.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source /usr/miniconda3/etc/profile.d/conda.sh
+conda activate unidexgrasp
+
+export PATH=/public/software/CUDA/cuda-11.4/bin${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=/public/software/CUDA/cuda-11.4/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+
+nvcc --version
+nvidia-smi
+
+cd /public/home/v-liuym/projects/UniDexGrasp/dexgrasp_generation
+
+python ./network/train.py --config-name glow_config \
+                          --exp-dir ./glow_train
